9f8ed208176c7451f0878041885384cf7ba33e04
BrandonC123 <brandoc318@gmail.com>
2022-07-20 16:43:41 -0700

    Initialize firebase and test database.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import "./App.css";
+import db from ".";
+import { doc, getDoc } from "firebase/firestore";
 import GameBoard from "./components/GameBoard";
+import Footer from "./components/Footer";
 
 function App() {
+    const test = doc(db, "image-position", "object-1");
+    async function tester() {
+        const pos = await getDoc(test);
+        console.log(pos.data());
+    }
+    tester();
     return (
         <div className="App">
             <GameBoard />
+            <Footer />
         </div>
     );
 }
--- src/index.js
+++ src/index.js
@@ -1,14 +1,33 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import { getFirestore } from "firebase/firestore";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>
 );
+
+const firebaseConfig = {
+    apiKey: "AIzaSyD4XUC9UR-tCpAAnoNP62X7cmv3RfpzpzA",
+    authDomain: "where-s-waldo-eb5d1.firebaseapp.com",
+    projectId: "where-s-waldo-eb5d1",
+    storageBucket: "where-s-waldo-eb5d1.appspot.com",
+    messagingSenderId: "189216827741",
+    appId: "1:189216827741:web:91cf46a65d333266fde0b4",
+    measurementId: "G-42Z97WC3H7",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app)
+export default db;
+const analytics = getAnalytics(app);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
