26a15d176f43081f53cd788765f9744bb11e8cd7
BrandonC123 <brandoc318@gmail.com>
2022-07-22 23:07:34 -0700

    Refactor getting data from database and changed checking matches to percentages instead of fixed values.

--- src/components/GameBoard.jsx
+++ src/components/GameBoard.jsx
@@ -5,8 +5,8 @@ import Dropdown from "./Dropdown";
 
 const GameBoard = ({ index, imgSrc }) => {
     const nameArrays = [
-        ["Waldo1", "test", "test"],
-        ["Waldo2", "test", "test"],
+        ["Waldo", "Buff Guy", "Wizard"],
+        ["Waldo", "Backpacker", "Rag Guy"],
     ];
     const [testArray, setTestArray] = useState([]);
     useEffect(() => {
@@ -19,10 +19,12 @@ const GameBoard = ({ index, imgSrc }) => {
     const [userClick, setUserClick] = useState([0, 0]);
 
     async function fillTestArray() {
+        let tempArray = [];
         const querySnapshot = await getDocs(collection(db, "image-position-1"));
         querySnapshot.forEach((doc) => {
-            setTestArray([...testArray, doc.data()]);
+            tempArray.push(doc.data());
         });
+        setTestArray(tempArray);
     }
 
     function toggleDropdown(cursorX, cursorY) {
@@ -31,7 +33,6 @@ const GameBoard = ({ index, imgSrc }) => {
     }
     useEffect(() => {
         if (display) {
-            console.log(cursorPosition[1]);
             document.querySelector(
                 ".dropdown"
             ).style.left = `${cursorPosition[0]}px`;
@@ -41,27 +42,26 @@ const GameBoard = ({ index, imgSrc }) => {
         }
     }, [display, cursorPosition]);
     function checkMatch(userSelection) {
-        let match = false;
         const testObject = {
             x: userClick[0],
             y: userClick[1],
             choice: userSelection,
         };
-        console.log(testObject);
-        testArray.forEach((object) => {
-            console.log(object);
+        for (let i = 0; i < testArray.length; i++) {
+            const object = testArray[i];
             if (testObject.x >= object.x1 && testObject.x <= object.x2) {
                 console.log("layer 1");
                 if (testObject.y >= object.y1 && testObject.y <= object.y2) {
                     console.log("layer 2");
                     if (testObject.choice === object.name) {
                         console.log("layer 3");
-                        match = true;
                         console.log("correct!");
+                        setDisplay(!display);
+                        return;
                     }
                 }
             }
-        });
+        }
         // Close dropdown after choosing a character
         setDisplay(!display);
     }
@@ -77,8 +77,11 @@ const GameBoard = ({ index, imgSrc }) => {
                 onClick={(event) => {
                     toggleDropdown(event.pageX, event.pageY);
                     const boxCoord = event.target.getBoundingClientRect();
-                    const x = Math.round(event.clientX - boxCoord.left);
-                    const y = Math.round(event.clientY - boxCoord.top);
+                    let x = Math.round(event.clientX - boxCoord.left);
+                    x = Math.round((x / boxCoord.width) * 100);
+                    let y = Math.round(event.clientY - boxCoord.top);
+                    y = Math.round((y / boxCoord.height) * 100);
+                    console.log(x, y);
                     setUserClick([x, y]);
                 }}
                 className="gameboard-img"
