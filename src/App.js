013d1edb24a75ecbaf405c57ebceae0839a077fc
BrandonC123 <brandoc318@gmail.com>
2022-07-24 12:43:53 -0700

    Add imgSrc array . Positioned timer and character menu

--- src/App.js
+++ src/App.js
@@ -3,9 +3,24 @@ import GameBoard from "./components/GameBoard";
 import Footer from "./components/Footer";
 import { useEffect, useState } from "react";
 import Header from "./components/Header";
+import Timer from "./components/Timer";
+import CharacterMenu from "./components/CharacterMenu";
 
 function App() {
+    document.title = "Where's Waldo";
     const [imgSrc, setImgSrc] = useState("/img/gameboard-1.jpg");
+    const menuImgSrcArray = [
+        [
+            { name: "Waldo", imgSrc: "/img/waldo1.jpg" },
+            { name: "Buff Guy", imgSrc: "/img/buff-guy.jpg" },
+            { name: "Wizard", imgSrc: "/img/wizard.jpg" },
+        ],
+        [
+            { name: "Waldo", imgSrc: "/img/waldo2.jpg" },
+            { name: "Backpacker", imgSrc: "/img/backpacker.jpg" },
+            { name: "Rag Guy", imgSrc: "/img/rag-guy.jpg" },
+        ],
+    ];
     const [gameboardIndex, setGameBoardIndex] = useState(1);
 
     return (
@@ -14,6 +29,8 @@ function App() {
                 setImgSrc={setImgSrc}
                 setGameBoardIndex={setGameBoardIndex}
             />
+            <Timer />
+            <CharacterMenu imgArray={menuImgSrcArray[gameboardIndex - 1]} />
             <GameBoard index={gameboardIndex} imgSrc={imgSrc} />
             <Footer />
         </div>
