866946477d1faa5266625ce4c288c039ba3b7fbe
BrandonC123 <brandoc318@gmail.com>
2022-07-24 18:26:01 -0700

    Create timer

--- src/components/Timer.jsx
+++ src/components/Timer.jsx
@@ -1,37 +1,56 @@
 import { useEffect, useState } from "react";
 
-const Timer = () => {
-    const [time, setTime] = useState(0);
-    const [start, setStart] = useState(false);
+const Timer = ({ time, setTime, start, startGame }) => {
+    const [ms, setMs] = useState(0);
+    const [sec, setSec] = useState(0);
+    const [min, setMin] = useState(0);
+    // const [time, setTime] = useState("00:00:00");
+    // const [start, setStart] = useState(false);
     useEffect(() => {
-        let timer;
         if (start) {
-            timer = setInterval(() => {
-                setTime(time + 1);
-            }, 1000);
+            var timer = setInterval(() => {
+                setMs(ms + 1);
+                convertTime();
+            }, 10);
         }
         return () => {
             clearInterval(timer);
         };
-    }, [time, start]);
+    }, [ms, sec, min, start]);
+    function convertTime() {
+        if (ms === 100) {
+            setMs(0);
+            setSec(sec + 1);
+        }
+        if (sec === 60) {
+            setSec(0);
+            setMin(min + 1);
+        }
+        setTime(
+            `${min.toString().padStart(2, "0")}:${sec
+                .toString()
+                .padStart(2, "0")}:${ms.toString().padStart(2, "0")}`
+        );
+    }
 
     return (
         <div className="timer-container">
-            Timer: {time} <div></div>
+            Timer: {time}
+            <div></div>
             <button
                 onClick={() => {
-                    setStart(true);
+                    startGame();
                 }}
             >
                 Start
             </button>
-            <button
+            {/* <button
                 onClick={() => {
                     setStart(false);
                 }}
             >
                 Stop
-            </button>
+            </button> */}
         </div>
     );
 };
